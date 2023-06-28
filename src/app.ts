8e533e064b6ad8a84948335c3a741b0d6dc3ac4b
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-06-28 20:44:37 +0600

    user create successfully

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRouter from './app/modules/user/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,6 +8,10 @@ app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// api 
+
+app.use("/api/v1/user" , userRouter)
 
 //Testing
 app.get('/', (req: Request, res: Response) => {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,20 @@
+import config from '../../../config'
 import { IUser } from './user.interface'
 import { User } from './user.model'
+import { generateUser } from './user.utils'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
+  const id = await generateUser()
+  user.id = id
+  if(!user.password){
+    user.password = config.defult_user_password as string
+  }
   const createUser = await User.create(user)
   if (!createUser) {
     throw new Error('Failed to create user !')
   }
   return createUser
 }
+ export default {
+    createUser
+ }
