1144232135f90edcf4a9844bc68cf759ddf62b16
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-06-28 11:26:49 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,3 @@
-// import mongoose from "mongoose";
-// import config from "./config/index";
-// import app from "./app";
-
-// async function boostrap() {
-//     try{
-//         await mongoose.connect(config.database as string);
-//         console.log("Data Base Connection SuccessFully");
-
-//         app.listen(config.port , () =>{
-//             console.log("sarver start")
-//         })
-        
-
-//     }catch(e){
-//         console.log("Fail to data connection")
-
-
-//     }
-   
-  
-
-//   }
-
-//   boostrap() 
-
 import mongoose from "mongoose";
 import config from "./config/index";
 import app from "./app";
