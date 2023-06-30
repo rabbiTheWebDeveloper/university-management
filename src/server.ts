f16036a43e192d2c4ae0295b970500777a04c7c5
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-06-30 11:35:25 +0600

    loger set and error solve

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
+import { logger } from './shared/logger'
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`🛢   Database is connected successfully`)
+    logger.info(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
+      logger.info(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
     console.log('Failed to connect database', err)
