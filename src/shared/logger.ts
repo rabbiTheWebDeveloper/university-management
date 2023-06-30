1490b8b37fcc660fccd452648a92c1e32df4fb57
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-06-30 12:15:29 +0600

    update

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,43 +1,43 @@
-import { createLogger, format, transports } from 'winston'
-import path from 'path'
-import DailyRotateFile from 'winston-daily-rotate-file';
+// import { createLogger, format, transports } from 'winston'
+// import path from 'path'
+// import DailyRotateFile from 'winston-daily-rotate-file';
 
 
-const { combine, timestamp, label, printf } = format
+// const { combine, timestamp, label, printf } = format
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
+// const myFormat = printf(({ level, message, label, timestamp }) => {
 
-    const date = new Date(timestamp)
-    const hour = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-  return `${date.toDateString()} ${hour}:${minutes}:${seconds} } [${label}] ${level}: ${message}`
-})
+//     const date = new Date(timestamp)
+//     const hour = date.getHours()
+//     const minutes = date.getMinutes()
+//     const seconds = date.getSeconds()
+//   return `${date.toDateString()} ${hour}:${minutes}:${seconds} } [${label}] ${level}: ${message}`
+// })
 
-const logger = createLogger({
-  level: 'info',
-  format:  combine(
-    label({ label: 'ph!' }),
-    timestamp(),
-    myFormat
-  ),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-        filename:path.join(
-            process.cwd(),
-            'logs',
-            'winston',
-            'successes',
-            'phu-%DATE%-success.log'
-          ),
-        datePattern: 'YYYY-DD-MM-HH',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d'
-      })
+// const logger = createLogger({
+//   level: 'info',
+//   format:  combine(
+//     label({ label: 'ph!' }),
+//     timestamp(),
+//     myFormat
+//   ),
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//         filename:path.join(
+//             process.cwd(),
+//             'logs',
+//             'winston',
+//             'successes',
+//             'phu-%DATE%-success.log'
+//           ),
+//         datePattern: 'YYYY-DD-MM-HH',
+//         zippedArchive: true,
+//         maxSize: '20m',
+//         maxFiles: '14d'
+//       })
 
-  ],
-})
+//   ],
+// })
 
-export { logger }
+// export { logger }
