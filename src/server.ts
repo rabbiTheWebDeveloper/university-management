8f000b4b58ab91bbd8d755510f047c07bb8b6588
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-06-30 12:10:19 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
-import { logger } from './shared/logger'
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    logger.info(`🛢   Database is connected successfully`)
+    console.log(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
-      logger.info(`Application  listening on port ${config.port}`)
+      console.log(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
     console.log('Failed to connect database', err)
